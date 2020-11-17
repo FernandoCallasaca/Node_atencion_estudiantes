1abd67625aeaddca22035ac08dc6fa354160f662
Fernando Callasaca <fernandocallasaca@outlook.com>
2020-11-17 14:14:08 -0500

    Agregando dependencias y ahora se suben los archivos al directorio del node

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const express = require('express')
 var jwt = require('jsonwebtoken')
 var bodyParser = require('body-parser')
 var cors = require('cors');
+var multer = require('multer');
+const fs = require('fs');
 
 const dbSeguridad = require('./dal/seguridad')
 const dbGeneral = require('./dal/general')
 
 const app = express()
 const port = 3200
+
+const ruta = '/archivos/proyectos';
 
 app.use(bodyParser.json({ limit: '1000mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '1000mb' }));
@@ -31,9 +35,40 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.use('/static', express.static(__dirname + '/fotos/encuesta'));
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, req.query.c_ruta);
+    },
+    filename: function (req, file, callback) {
+        callback(null, req.query.c_nombre);
+    }
+});
+var upload = multer({ storage: storage }).single('DA');
 
+app.use('/static', express.static(__dirname + ruta));
 
+app.post('/api/general/uploadfile', function (req, res) {
+    let proyecto = req.query.proyecto;
+    let dir = __dirname.replace('\dal', '') + "/archivos/proyectos/" + proyecto + "/";
+    //let c_nombre = 'DA-' + Date.now() + '.' + req.query.extension;
+    let c_nombre =   req.query.extension;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, 0744);
+    }
+
+    req.query.c_ruta = dir;
+    req.query.c_nombre = c_nombre;
+    dir = dir + '' + c_nombre;
+    upload(req, res, function (err) {
+
+        if (err) {
+            res.status(200).json({ estado: false, mensaje: "No se pudo cargar el archivo: " + err.stack, data: null })
+        } else {
+
+        res.status(200).json({ estado: true, mensaje: "Archivo cargado", c_ruta: c_nombre })
+        }
+    });
+})
 
 /* General */
 app.post('/api/general/getestudiante', dbGeneral.getEstudiante)
